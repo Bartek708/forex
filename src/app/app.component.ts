dc349f20cc96e79cc275fbbc048661fb1b627deb
bartek708 <bartek_starwars@hotmail.com>
2019-03-28 11:24:33 +0100

    forex-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Exchange } from './exchange'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forex-app';
+  results: Exchange[];
+  interval: number;
+  timer: number;
+  progress: number;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.refreshData();
+    this.progress = 0;
+    if(this.interval){
+    	clearInterval(this.interval);
+    }
+    this.interval = setInterval(() => {
+    	this.refreshData();
+    	this.progress = 0;
+    }, 10000);
+
+    if (this.timer) {
+    	clearInterval(this.timer);
+    }
+    this.timer = setInterval(() => {
+    	this.progress += 5;
+    }, 500);
+  }
+
+  refreshData(): void {
+  	this.http.get<Exchange[]>("https://forex.1forge.com/1.0.3/quotes?&api_key=9bW5urPmwxXJnFB2hxzyfmi7JX9kuIUN")
+  	.subscribe(
+  		data => {
+  		this.results = data;
+  	},
+  	err => {console.log("Error fetching data.")});
+  }
 }
